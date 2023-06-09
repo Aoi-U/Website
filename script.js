864edf0b93b1f0ebd4cb717ae82b71bb8edd9168
Aoi-U <aoilovepokemon@gmail.com>
2023-06-08 18:25:37 -0600

    Added a navigation menu icon

---
+++ script.js
@@ -0,0 +1,9 @@
+function animateMenu() {
+    let bar1 = document.getElementById("menu-bar1");
+    let bar3 = document.getElementById("menu-bar3");
+    let bar2Left = document.getElementById("menu-bar2-left");
+    let bar2Right = document.getElementById("menu-bar2-right");
+
+    bar1.classList.toggle("bar1-change");
+    bar3.classList.toggle("bar3-change");
+}
